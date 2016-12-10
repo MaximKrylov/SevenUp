e4862501065bc04191ce9c19d1b31fc87c6eba46
MaximKrylov <maxim.kryloff@mail.ru>
2016-12-10 20:33:52 +0300

    Added header.

--- src/app/components/home/home_controller.js
+++ src/app/components/home/home_controller.js
@@ -2,6 +2,5 @@
 
 angular.module('SevenUp')
     .controller('HomeController', ['$scope', function($scope) {
-        $scope.Name = 'SevenUp v1.0';
         $scope.Description = 'Simple programming language...';
     }]);
